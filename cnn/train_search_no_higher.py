4b0d29a14db8694ef45809d741bc2ef189c7ab8b
Mirofil <kawgawr@gmail.com>
2021-07-15 17:12:15 +0100

    Small cahnges

--- cnn/train_search_no_higher.py
+++ cnn/train_search_no_higher.py
@@ -439,4 +439,14 @@ def train(
 
 
 if __name__ == "__main__":
+    
+    if 'TORCH_HOME' not in os.environ:
+        print("Changing os environ")
+        if os.path.exists('/storage/.torch/'):
+            os.environ["TORCH_HOME"] = '/storage/.torch/'
+
+        gdrive_torch_home = "/content/drive/MyDrive/colab/data/TORCH_HOME"
+
+        if os.path.exists(gdrive_torch_home):
+            os.environ["TORCH_HOME"] = "/content/drive/MyDrive/colab/data/TORCH_HOME"
     main()
